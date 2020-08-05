a9a7fdbcbdadb9c4467420e63592a58d5dfd8f83
Ethan Hu <fendouhyz@163.com>
2020-08-05 15:37:33 +0800

    chore: delete the "title:" in the front of file

--- src/remote-markdown.js
+++ src/remote-markdown.js
@@ -22,7 +22,11 @@ export function install(hook, vm) {
     if (result && result[1]) {
       const targetFile = result[1];
       getFile(targetFile)
-        .then((data) => next(content.replace(reg, `\n ${data} \n`)))
+        .then((data) => {
+          const index = data.indexOf("---", 4);
+          const data_after = data.substring(index + 4);
+          return next(content.replace(reg, `\n ${data_after} \n`))
+        })
         .catch((err) => console.error(err));
     } else {
       next(content);
